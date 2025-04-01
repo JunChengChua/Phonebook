001261781796e30e6e73e075222c53562b355521
Harsh <harsharma1210@gmail.com>
2025-03-31 20:24:53 -0400

    Rudimentary start

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,22 @@ import Box from './Box';
 
 function App() {
     return (
-        <div className="flex flex-col items-center bg-gradient-to-r from-[#30cfd0] to-[#330867]">
-            <h1 className="font-bold text-4xl">Hello World</h1> 
+        <div className="flex flex-col bg-gradient-to-b from-[#FFFEFF] to-[#6ad4fb] min-h-screen w-full bg-auto"> 
+            <div className="flex flex-row">
+                <div>
+                    <img src="images\TBHC Logo.jpg"/>
+                </div>
+                The Header
+            </div>
+            <div>
+                The Search Bar and Button to Submit
+            </div>
         </div>
     )
 }
 
 export default App;
+
+/*
+background-image: linear-gradient(-225deg, #2CD8D5 0%, #C5C1FF 56%, #FFBAC3 100%);
+*/
