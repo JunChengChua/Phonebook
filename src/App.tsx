9daea23604d0c7a8b3501603d8096d451477704b
Harsh <harsharma1210@gmail.com>
2025-04-02 14:13:13 -0400

    Rudimentary front page

--- src/App.tsx
+++ src/App.tsx
@@ -4,15 +4,19 @@ import Box from './Box';
 
 function App() {
     return (
-        <div className="flex flex-col bg-gradient-to-b from-[#FFFEFF] to-[#6ad4fb] min-h-screen w-full bg-auto"> 
-            <div className="flex flex-row">
-                <div>
-                    <img src="images\TBHC Logo.jpg"/>
-                </div>
-                The Header
+        <div className="flex flex-col bg-gradient-to-b from-[#FFFEFF] from-35% to-[#6ad4fb] min-h-screen w-full bg-auto"> 
+            <div className="flex flex-row items-center gap-[0.5vw] pl-[1vw]">
+                <img className="h-[4vw]" src="images\TBHC Logo.png"/>
+                <h1 className="flex font-bold text-[3vw]">
+                    Phonebook
+                </h1>
             </div>
-            <div>
-                The Search Bar and Button to Submit
+            <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 flex flex-row gap-[1vh]">
+                <input className="px-4 py-2 border border-black rounded-xl focus:outline-none focus:ring-2 focus:ring-[#6af1fb] 
+                                  focus:border-transparent w-[25vw]" type="text" placeholder="Enter partial or full name..."/>
+                <button className="bg-[#2a2d2d] text-white px-4 py-2 rounded-lg hover:bg-[#6af1fb] hover:text-black transition">
+                    Search
+                </button>
             </div>
         </div>
     )
@@ -20,6 +24,4 @@ function App() {
 
 export default App;
 
-/*
-background-image: linear-gradient(-225deg, #2CD8D5 0%, #C5C1FF 56%, #FFBAC3 100%);
-*/
+/*6ad4fb Original Value*/
