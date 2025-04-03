94a6030149044dfa55209cd218f3f6272f8201cd
Harsh <harsharma1210@gmail.com>
2025-04-03 14:17:08 -0400

    Initial Commit to my own branch

--- src/App.tsx
+++ src/App.tsx
@@ -5,21 +5,51 @@ import Box from './Box';
 function App() {
     return (
         <div className="flex flex-col bg-gradient-to-b from-[#FFFEFF] from-35% to-[#6ad4fb] min-h-screen w-full bg-auto"> 
-            <div className="flex flex-row items-center gap-[0.5vw] pl-[1vw]">
-                <img className="h-[4vw]" src="images\TBHC Logo.png"/>
-                <h1 className="flex font-bold text-[3vw]">
-                    Phonebook
-                </h1>
+            {/* Header Section */}
+            <div className="grid grid-cols-3 items-center"> 
+                {/* Logo & Name */}
+                <div className="flex flex-row gap-[0.5vw] px-[1vw] py-4 items-center">
+                    <img className="h-[4vw]" src="images/TBHC Logo.png" alt="Logo"/>
+                    <h1 className="font-bold text-[3vw]">Phonebook</h1>
+                </div>
+                
+                {/* Search Bar */}
+                <div className="flex flex-row gap-[1vh]">
+                    <input 
+                        className="px-4 py-2 border border-black rounded-xl focus:outline-none focus:ring-2 focus:ring-[#6af1fb] 
+                                  focus:border-transparent w-[25vw]" 
+                        type="text" 
+                        placeholder="Enter partial or full name..."
+                    />
+                    <button className="bg-[#2a2d2d] text-white px-4 py-2 rounded-lg hover:bg-[#6af1fb] hover:text-black transition">
+                        Search
+                    </button>
+                </div>
+                
+                {/* Extra Div To Center The Elements */}
+                <div> 
+                </div>
             </div>
-            <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 flex flex-row gap-[1vh]">
-                <input className="px-4 py-2 border border-black rounded-xl focus:outline-none focus:ring-2 focus:ring-[#6af1fb] 
-                                  focus:border-transparent w-[25vw]" type="text" placeholder="Enter partial or full name..."/>
-                <button className="bg-[#2a2d2d] text-white px-4 py-2 rounded-lg hover:bg-[#6af1fb] hover:text-black transition">
-                    Search
-                </button>
+            
+            {/* Body */}
+            <div className="flex flex-grow flex-row">
+                {/* Filters */}
+                <div>
+
+                </div>
+                
+                {/* Main Table */}
+                <div>
+
+                </div>
+                
+                {/* Collapsable Information Secton */}
+                <div>
+
+                </div>
             </div>
         </div>
-    )
+    );
 }
 
 export default App;
